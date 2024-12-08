972261c57077d2a4d5fc813de30d99a0ea4694c6
JonasVanRoy6 <147380123+JonasVanRoy6@users.noreply.github.com>
2024-12-08 16:29:31 +0100

    Update order.js

--- src/models/order.js
+++ src/models/order.js
@@ -3,11 +3,21 @@ const mongoose = require('mongoose');
 
 // Define the Order schema
 const orderSchema = new mongoose.Schema({
-    product: { type: String, required: true },
-    quantity: { type: Number, required: true },
+    configuration: {
+        inside: { type: String, required: true },
+        mat_laces: { type: String, required: true },
+        mat_outside_1: { type: String, required: true },
+        mat_outside_2: { type: String, required: true },
+        mat_outside_3: { type: String, required: true },
+    },
+    customer: {
+        name: { type: String, required: true },
+        address: { type: String, required: true },
+        email: { type: String, required: true, match: /.+@.+..+/ },
+    },
     price: { type: Number, required: true },
     status: { type: String, enum: ['pending', 'shipped', 'delivered'], default: 'pending' },
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
 });
 
 // Create and export the Order model
